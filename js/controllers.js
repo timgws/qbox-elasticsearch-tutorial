3138f2e59e9f563a28fa1b76ae11c1023a236b15
OneMT <mr.michael.lussier@gmail.com>
2014-04-15 01:24:06 -0500

    fixed typo in controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1,18 +1,18 @@
 var myControllers = angular.module('myControllers', []);
 
-myControllerstrollers.controller('SearchController', ['$scope', 'es','searchService',
-                                 function($scope, es, searchService) {
-                                   $scope.search = function() {
+myControllers.controller('SearchController', ['$scope', 'es','searchService',
+                               function($scope, es, searchService) {
+                                 $scope.search = function() {
 
-                                     var hits = searchService.textSearch($scope.search.matchInput, $scope.search.termFilter);
+                                   var hits = searchService.textSearch($scope.search.matchInput, $scope.search.termFilter);
 
-                                     hits.then(function(resp) {
-                                       $scope.results = resp.hits.hits;
-                                       $scope.sports = ['Baseball', 'Football','Hockey','Basketball', 'Golf']
-                                     });
-                                   };
-
-                                   angular.element(document).ready(function () {
-                                     $scope.search();
+                                   hits.then(function(resp) {
+                                     $scope.results = resp.hits.hits;
+                                     $scope.sports = ['Baseball', 'Football','Hockey','Basketball', 'Golf']
                                    });
-                                 }]);
+                                 };
+
+                                 angular.element(document).ready(function () {
+                                   $scope.search();
+                                 });
+                               }]);
